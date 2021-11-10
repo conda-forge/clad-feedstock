13d3d73514c3559ea6883099bcfeeb2695be9fe5
Ioana Ifrim <ioana.ifrim@gmail.com>
2021-11-10 15:22:08 +0200

    Fix auto-commit typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,8 +11,8 @@ make install
 
 if [[ ${clangdev} == '5.*' ]]; then
     echo "Making xeus-cling based Jupyter kernels"
-    mkdir -p $PREFIX</span>/share/jupyter/kernels/
-    cp -r $RECIPE_DIR/kernels/* $PREFIX</span>/share/jupyter/kernels/
+    mkdir -p $PREFIX/share/jupyter/kernels/
+    cp -r $RECIPE_DIR/kernels/* $PREFIX/share/jupyter/kernels/
 else
     echo "Not making Jupyter kernels"
 fi
