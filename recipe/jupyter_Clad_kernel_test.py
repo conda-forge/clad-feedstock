ca10c3cbd8d82ba9c1bb8d55b9ef8da262a46e3a
Ioana Ifrim <ioana.ifrim@gmail.com>
2021-11-11 09:36:51 +0200

    Test with normal kernel

--- recipe/jupyter_Clad_kernel_test.py
+++ recipe/jupyter_Clad_kernel_test.py
@@ -4,7 +4,7 @@ import jupyter_kernel_test
 
 class CladXCppTests(jupyter_kernel_test.KernelTests):
 
-    kernel_name = 'xcpp14-Clad'
+    kernel_name = 'xcpp14'
 
     # language_info.name in a kernel_info_reply should match this
     language_name = 'c++'
@@ -12,13 +12,20 @@ class CladXCppTests(jupyter_kernel_test.KernelTests):
     # Code in the kernel's language to write "hello, world" to stdout
     code_hello_world = '#include <iostream>\nstd::cout << "hello, world" << std::endl;'''
 
-   # Code for Clad forward mode testing 
     code_execute_result = [
         {
-            'code': '#include "clad/Differentiator/Differentiator.h"\ndouble f(double x) {return x*x;};\nauto f_g = clad::differentiate(f);\nf_g.execute(2)',
-            'result': '34.0000000'
+            'code': '2*3',
+            'result': '6'
         }
     ]
+
+#    # Code for Clad forward mode testing 
+#     code_execute_result = [
+#         {
+#             'code': '#include "clad/Differentiator/Differentiator.h"\ndouble f(double x) {return x*x;};\nauto f_g = clad::differentiate(f);\nf_g.execute(2)',
+#             'result': '34.0000000'
+#         }
+#     ]
 
 if __name__ == '__main__':
     unittest.main()
