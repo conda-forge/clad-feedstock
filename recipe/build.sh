923ec918cbb10d96cbe00dd249ed024ffcae314b
ioanaif <ioana.ifrim@gmail.com>
2023-07-19 16:57:49 -0400

    Comment out for now the xeus-cling requirements from recipe since 5. is no longer supported

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,14 +9,12 @@ cmake ${CMAKE_ARGS} \
 make -j${CPU_COUNT}
 make install
 
-#there is no support for 5. anymore, uncomment this when xeus-cling will work with 9.
-
-# if [[ ${clangdev} == '5.*' ]]; then
-#     echo "Making xeus-cling based Jupyter kernels"
-#     mkdir -p $PREFIX/share/jupyter/kernels/
-#     cp -r $RECIPE_DIR/kernels/* $PREFIX/share/jupyter/kernels/
-#     sed -i "s#@PREFIX@#$PREFIX#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
-#     sed -i "s#@SHLIB_EXT@#$SHLIB_EXT#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
-# else
-#     echo "Not making Jupyter kernels"
-# fi
+if [[ ${clangdev} == '5.*' ]]; then
+    echo "Making xeus-cling based Jupyter kernels"
+    mkdir -p $PREFIX/share/jupyter/kernels/
+    cp -r $RECIPE_DIR/kernels/* $PREFIX/share/jupyter/kernels/
+    sed -i "s#@PREFIX@#$PREFIX#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
+    sed -i "s#@SHLIB_EXT@#$SHLIB_EXT#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
+else
+    echo "Not making Jupyter kernels"
+fi
