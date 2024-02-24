df00dfe34ece25a636fb438a502c5ea16f5215da
Alexander Penev <alexander.p.penev@gmail.com>
2024-02-24 07:59:10 +0000

    Adjust the conda recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,18 @@
 mkdir build
 cd build
 
+export CONDA_BUILD_SYSROOT=$CONDA_PREFIX/$HOST/sysroot
+#echo "#include <vector>" | clang++ --gcc-toolchain=$CONDA_PREFIX -xc++ - -v
+
 cmake ${CMAKE_ARGS} \
+      -DCMAKE_SYSROOT=$CONDA_BUILD_SYSROOT \
       $SRC_DIR/source
 
-make -j${CPU_COUNT}
+make VERBOSE=1
+#make -j${CPU_COUNT}
 make install
 
-if [[ ${clangdev} == '5.*' ]]; then
+if [[ ${clangdev} == '13.*' ]]; then
     echo "Making xeus-cling based Jupyter kernels"
     mkdir -p $PREFIX/share/jupyter/kernels/
     cp -r $RECIPE_DIR/kernels/* $PREFIX/share/jupyter/kernels/
