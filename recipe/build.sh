3e51b7b1ccb69b45d69c864219a58821d26e3ec8
Alexander Penev <alexander.p.penev@gmail.com>
2024-02-28 06:37:18 +0000

    Debug 5

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,6 +12,11 @@ if [[ "$(uname)" == "Linux"* ]]; then
   export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/$HOST/include/c++/12.3.0:$CONDA_PREFIX/$HOST/include/c++/12.3.0/$HOST
 fi
 
+if [[ "${uname}" == "Darwin"* ]]; then
+  # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+  CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake ${CMAKE_ARGS} \
       -DCMAKE_SYSROOT=$CONDA_BUILD_SYSROOT \
       $SRC_DIR/source
@@ -20,14 +25,12 @@ make VERBOSE=1
 #make -j${CPU_COUNT}
 make install
 
-#if [[ ${clangdev} == '13.*' ]]; then
-#    echo "Making xeus-cling based Jupyter kernels"
-#    mkdir -p $PREFIX/share/jupyter/kernels/
-#    cp -r $RECIPE_DIR/kernels/* $PREFIX/share/jupyter/kernels/
-#    sed -i "s#@PREFIX@#$PREFIX#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
-#    sed -i "s#@SHLIB_EXT@#$SHLIB_EXT#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
-#else
-#    echo "Not making Jupyter kernels"
-#fi
-
-exit 0
+if [[ ${clangdev} == '13.*' ]]; then
+    echo "Making xeus-cling based Jupyter kernels"
+    mkdir -p $PREFIX/share/jupyter/kernels/
+    cp -r $RECIPE_DIR/kernels/* $PREFIX/share/jupyter/kernels/
+    sed -i "s#@PREFIX@#$PREFIX#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
+    sed -i "s#@SHLIB_EXT@#$SHLIB_EXT#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
+else
+    echo "Not making Jupyter kernels"
+fi
