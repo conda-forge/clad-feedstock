dfcf80c0956c8805208f040e955054e06cc1cd46
Alexander Penev <alexander.p.penev@gmail.com>
2024-03-23 20:10:12 +0000

    Fix package

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,12 +23,14 @@ cmake ${CMAKE_ARGS} \
 make -j${CPU_COUNT}
 make install
 
-if [[ ${clangdev} == '13.*' ]]; then
-    echo "Making xeus-cling based Jupyter kernels"
-    mkdir -p $PREFIX/share/jupyter/kernels/
-    cp -r $RECIPE_DIR/kernels/* $PREFIX/share/jupyter/kernels/
-    sed -i "s#@PREFIX@#$PREFIX#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
-    sed -i "s#@SHLIB_EXT@#$SHLIB_EXT#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
-else
-    echo "Not making Jupyter kernels"
-fi
+#if [[ ${clangdev} == '13.*' ]]; then
+echo "Making xeus-cling based Jupyter kernels"
+mkdir -p $PREFIX/share/jupyter/kernels/
+cp -r $RECIPE_DIR/kernels/* $PREFIX/share/jupyter/kernels/
+sed -i "s#@PREFIX@#$PREFIX#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
+sed -i "s#@SHLIB_EXT@#$SHLIB_EXT#g" $PREFIX/share/jupyter/kernels/*-Clad/*.json
+#else
+#    echo "Not making Jupyter kernels"
+#fi
+
+exit 0
