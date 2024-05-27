9ec3185c417a1f7f1c0fc6070915f4e3974aebae
Alexander Penev <alexander.p.penev@gmail.com>
2024-05-27 15:08:04 +0000

    Try to fix 12

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,6 +32,7 @@ if [[ "$(uname)" == "Darwin"* ]]; then
   if [[ "$clangdev" == "10.*" ]]; then
     echo "@@@B"
     export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/lib/clang/10.0.1/include:$CPLUS_INCLUDE_PATH
+    export CXXFLAGS="$CXXFLAGS -I$CONDA_PREFIX/lib/clang/10.0.1/include"
     ls -la $CONDA_PREFIX/lib/clang/10.0.1/include
     cat $CONDA_PREFIX/lib/clang/10.0.1/include/stddef.h
   fi
