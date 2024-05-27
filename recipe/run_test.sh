344461ac4d5db159133113794b168d6df990965d
Alexander Penev <alexander.p.penev@gmail.com>
2024-05-27 10:59:59 +0000

    Try to fix 9

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -49,7 +49,7 @@ echo "#include <vector>" | clang $CXXFLAGS -fsyntax-only -xc++ - -v
 
 # Check if we can process a simple program.
 echo "@@@3"
-clang++ -v $CXXFLAGS -I$PREFIX/include -fplugin=$PREFIX/lib/clad${SHLIB_EXT} -osanity test.cpp
+clang -v $CXXFLAGS -xc++ -I$PREFIX/include -fplugin=$PREFIX/lib/clad${SHLIB_EXT} -osanity test.cpp
 ./sanity
 
 # Make sure we do not link anything llvm or clang related
