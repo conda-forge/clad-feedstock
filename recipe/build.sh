2784ad891084f19403c8d8f24706c9f1aa6308b6
Vassil Vassilev <v.g.vassilev@gmail.com>
2025-05-26 09:04:18 +0000

    Drop clang 8 and 9 and add 19 and 20.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,26 +5,6 @@ set -x
 mkdir build
 cd build
 
-if [[ "$(uname)" == "Linux"* ]]; then
-  if [[ "$clangdev" == "8.*" || "$clangdev" == "9.*" ]]; then
-    #export CONDA_BUILD_SYSROOT=$CONDA_PREFIX/$HOST/sysroot
-    #export CONDA_BUILD_SYSROOT=$PREFIX/$HOST/sysroot
-    GCCVERSION=$(basename $(dirname $($GXX -print-libgcc-file-name)))
-    export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/$HOST/include/c++/$GCCVERSION:$CONDA_PREFIX/$HOST/include/c++/$GCCVERSION/$HOST:$CONDA_PREFIX/$HOST/sysroot/usr/include:$CPLUS_INCLUDE_PATH
-    export C_INCLUDE_PATH=$CONDA_PREFIX/$HOST/usr/include:$C_INCLUDE_PATH
-    ln -s $CONDA_PREFIX/lib/gcc/$HOST/$GCCVERSION/crtbegin.o $CONDA_BUILD_SYSROOT/usr/lib/
-    ln -s $CONDA_PREFIX/lib/gcc/$HOST/$GCCVERSION/crtbeginS.o $CONDA_BUILD_SYSROOT/usr/lib/
-    ln -s $CONDA_PREFIX/lib/gcc/$HOST/$GCCVERSION/crtend.o $CONDA_BUILD_SYSROOT/usr/lib/
-    ln -s $CONDA_PREFIX/lib/gcc/$HOST/$GCCVERSION/crtendS.o $CONDA_BUILD_SYSROOT/usr/lib/
-    ln -s $CONDA_PREFIX/lib/gcc/$HOST/$GCCVERSION/libgcc.a $CONDA_BUILD_SYSROOT/usr/lib/
-    ln -s $CONDA_PREFIX/lib/libgcc_s.so $CONDA_BUILD_SYSROOT/usr/lib/
-    ln -s $CONDA_PREFIX/lib/libgcc_s.so.1 $CONDA_BUILD_SYSROOT/usr/lib/
-
-    export CFLAGS="$CFLAGS -isysroot $CONDA_BUILD_SYSROOT"
-    export CXXFLAGS="$CXXFLAGS -isysroot $CONDA_BUILD_SYSROOT"
-  fi
-fi
-
 if [[ "$(uname)" == "Darwin"* ]]; then
   # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
@@ -57,7 +37,7 @@ make -j${CPU_COUNT}
 if [[ "$(uname)" == "Linux"* ]]; then
   # The llvm-tools-8 package do not have FileCheck. Clad's test Misc/RunDemos.C
   # fails with clang-9.
-  if ! [[ "$clangdev" == "8.*" || "$clangdev" == "9.*" || "$clangdev" == "cling" ]]; then
+  if ! [[ "$clangdev" == "cling" ]]; then
       # Make FileCheck findable.
       ln -s $BUILD_PREFIX/libexec/llvm/FileCheck $BUILD_PREFIX/bin/FileCheck
 
