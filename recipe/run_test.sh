62833c15b16d8fcfcab149bd62cc2bcf0ded1799
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-10-17 19:05:59 +0100

    Try again to fix

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -39,7 +39,7 @@ echo "#include <vector>" | clang $CXXFLAGS -fsyntax-only -xc++ - -v
 
 # Check if we can process a simple program.
 find $PREFIX -iname "*lto*"
-clang++ $CXXFLAGS -xc++ -I$PREFIX/include -fplugin=$PREFIX/lib/clad${SHLIB_EXT} -osanity -lto_library test.cpp
+clang++ $CXXFLAGS -xc++ -I$PREFIX/include -fplugin=$PREFIX/lib/clad${SHLIB_EXT} -osanity -Wl,-lto_library,$PREFIX/lib/libLTO.dylib test.cpp
 ./sanity
 
 # Make sure we do not link anything llvm or clang related
