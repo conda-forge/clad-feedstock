76c96e34d8dda73cdb8a5267a5c70111abccfe80
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-10-18 21:37:24 +0100

    Try fix
    Removed LTO library checks and updated clang command.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -38,9 +38,7 @@ echo "" | clang $CXXFLAGS -fsyntax-only -xc++ - -v
 echo "#include <vector>" | clang $CXXFLAGS -fsyntax-only -xc++ - -v
 
 # Check if we can process a simple program.
-find $PREFIX -iname "*lto*"
-clang++ -Xlinker -lto_library -Xlinker $PREFIX/LIB/libLTO.dylib -flto -v test.cpp $CXXFLAGS -xc++ -I$PREFIX/include -fplugin=$PREFIX/lib/clad${SHLIB_EXT} -osanity test.cpp
-clang++ -Xlinker -lto_library -Xlinker $PREFIX/LIB/libLTO.dylib $CXXFLAGS -xc++ -I$PREFIX/include -fplugin=$PREFIX/lib/clad${SHLIB_EXT} -osanity test.cpp
+clang++ -fno-lto $CXXFLAGS -xc++ -I$PREFIX/include -fplugin=$PREFIX/lib/clad${SHLIB_EXT} -osanity test.cpp
 ./sanity
 
 # Make sure we do not link anything llvm or clang related
