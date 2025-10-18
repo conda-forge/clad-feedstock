c147872a6982ac47e911eefd55d95f8ff7ee77bc
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-10-18 21:52:00 +0100

    Add verbose flag to clang++ command in run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -38,7 +38,7 @@ echo "" | clang $CXXFLAGS -fsyntax-only -xc++ - -v
 echo "#include <vector>" | clang $CXXFLAGS -fsyntax-only -xc++ - -v
 
 # Check if we can process a simple program.
-clang++ -fno-lto $CXXFLAGS -xc++ -I$PREFIX/include -fplugin=$PREFIX/lib/clad${SHLIB_EXT} -osanity test.cpp
+clang++ -v $CXXFLAGS -xc++ -I$PREFIX/include -fplugin=$PREFIX/lib/clad${SHLIB_EXT} -osanity test.cpp
 ./sanity
 
 # Make sure we do not link anything llvm or clang related
